00ac2e1d0ffd27e591663a3d1df07aa564a2e70c
unknown <andreinichol.e.castro.dev@gmail.com>
2021-07-08 13:35:52 +0800

    fixed the spinner on first recipe click

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Results from "./components/Results/Results";
 import Recipe from "./components/Recipe/Recipe";
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import {useEffect} from "react";
+import {useSelector} from "react-redux";
 function App() {
-  const recipe = useSelector((state) => state.recipe.recipe);
+    useEffect(() => {
+        window.location.hash = "";
+    }, []);
 
-  const hasRecipe = Object.keys(recipe).length === 0;
-  useEffect(() => {
-    window.location.hash = "";
-  }, []);
-
-  return (
-    <div className="App">
-      <Header />
-      <Results />
-      {hasRecipe ? <p>Search Now : )</p> : <Recipe />}
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Results/>
+            <Recipe/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,140 +1,110 @@
 import React from "react";
-import {recipesActions} from "../../store/recipe-slice";
-import {ReactComponent as Bookmark} from "../../misc/svg/bookmark.svg";
-import {ReactComponent as BookmarkFill} from "../../misc/svg/bookmark-fill.svg"
-import {ReactComponent as Star} from "../../misc/svg/star.svg";
+import { recipesActions } from "../../store/recipe-slice";
+import { ReactComponent as Bookmark } from "../../misc/svg/bookmark.svg";
+import { ReactComponent as BookmarkFill } from "../../misc/svg/bookmark-fill.svg";
+import { ReactComponent as Star } from "../../misc/svg/star.svg";
 import Spinner from "../UI/Spinner/Spinner";
 import styles from "./Recipe.module.css";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 const Recipe = () => {
-    const recipe = useSelector((state) => state.recipe.recipe);
-    const bookmarkedItems = useSelector((state) => state.recipe.bookmarks);
+  const recipe = useSelector((state) => state.recipe.recipe);
+  const bookmarkedItems = useSelector((state) => state.recipe.bookmarks);
+  const hasRecipe = Object.keys(recipe).length === 0;
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-
-    const newRecipe = {
-        bookmarked: recipe.payload.bookmarked,
-        title: recipe.payload.title,
-        publisher: recipe.payload.publisher,
-        publisherUrl: recipe.payload.publisher_url,
-        imageUrl: recipe.payload.image_url,
-        recipeId: recipe.payload.recipe_id,
-        socialRank: recipe.payload.social_rank,
-        sourceUrl: recipe.payload.source_url,
-        ingredients: recipe.payload.ingredients
+  let newRecipe;
+  if (!hasRecipe) {
+    newRecipe = {
+      bookmarked: recipe.payload.bookmarked,
+      title: recipe.payload.title,
+      publisher: recipe.payload.publisher,
+      publisherUrl: recipe.payload.publisher_url,
+      imageUrl: recipe.payload.image_url,
+      recipeId: recipe.payload.recipe_id,
+      socialRank: recipe.payload.social_rank,
+      sourceUrl: recipe.payload.source_url,
+      ingredients: recipe.payload.ingredients,
     };
+  }
 
+  const newBookmarkedItems = bookmarkedItems.map((items) => {
+    return items.payload;
+  });
 
-    const newBookmarkedItems = bookmarkedItems.map((items) => {
-        return items.payload;
-    });
+  const bookmarkedItem = newBookmarkedItems.some(
+    (item) => item.recipeId === newRecipe.recipeId
+  );
 
-    const bookmarkedItem = newBookmarkedItems.some((item) => item.recipeId === newRecipe.recipeId);
+  const shouldRecipeSpin = useSelector((state) => state.loading.recipe);
 
-    const shouldRecipeSpin = useSelector((state) => state.loading.recipe);
-    
-    const addBookmarkHandler = () => {
-        dispatch(recipesActions.addOrRemoveBookmarks({payload: newRecipe}));
-    };
-    console.log(newRecipe.bookmarked)
-    return (
-        <React.Fragment> {
-            shouldRecipeSpin ? (
-                <Spinner/>) : (
-                <div className={
-                    styles.recipe
-                }>
-                    <img className={
-                            styles.recipeImg
-                        }
-                        src={
-                            newRecipe.imageUrl
-                        }
-                        alt=""/>
-                    <div className={
-                        styles.recipeTitleBlock
-                    }>
-                        <h2 className={
-                            styles.recipeTitle
-                        }>
-                            {
-                            newRecipe.title
-                        }</h2>
-                    </div>
-                    <div className={
-                        styles.recipeCookInfo
-                    }>
-                        <div className={
-                            styles.recipeTime
-                        }>
-                            <Star className={
-                                styles.recipeTimeSvg
-                            }/>
-                            <span>{
-                                newRecipe.socialRank
-                            }</span>
-                        </div>
-                        <div className={
-                            styles.recipeServing
-                        }>
-                            <button onClick={addBookmarkHandler}
-                                className={
-                                    styles.recipeBookmarkButton
-                            }>
-                                {
-                                bookmarkedItem ? <BookmarkFill className={
-                                    styles.recipeServingSvg
-                                }/> : <Bookmark className={
-                                    styles.recipeServingSvg
-                                }/>
-                            } </button>
-                        </div>
-                    </div>
+  const addBookmarkHandler = () => {
+    dispatch(recipesActions.addOrRemoveBookmarks({ payload: newRecipe }));
+  };
 
-                    <div className={
-                        styles.recipeIngredients
-                    }>
-                        <ul className={
-                            styles.recipeIngredientsList
-                        }>
-                            {
-                            newRecipe.ingredients.map((item, i) => (
-                                <li className={
-                                        styles.recipeList
-                                    }
-                                    key={i}>
-                                    <div className={
-                                        styles.recipeIngredient
-                                    }>
-                                        {item}</div>
-                                </li>
-                            ))
-                        } </ul>
-                    </div>
+  let recipeView;
+  if (!hasRecipe) {
+    recipeView = (
+      <div className={styles.recipe}>
+        <img className={styles.recipeImg} src={newRecipe.imageUrl} alt="" />
+        <div className={styles.recipeTitleBlock}>
+          <h2 className={styles.recipeTitle}>{newRecipe.title}</h2>
+        </div>
+        <div className={styles.recipeCookInfo}>
+          <div className={styles.recipeTime}>
+            <Star className={styles.recipeTimeSvg} />
+            <span>{Math.floor(newRecipe.socialRank)}</span>
+          </div>
+          <div className={styles.recipeServing}>
+            <button
+              onClick={addBookmarkHandler}
+              className={styles.recipeBookmarkButton}
+            >
+              {bookmarkedItem ? (
+                <BookmarkFill className={styles.recipeServingSvg} />
+              ) : (
+                <Bookmark className={styles.recipeServingSvg} />
+              )}{" "}
+            </button>
+          </div>
+        </div>
 
-                    <div className={
-                        styles.recipeDirections
-                    }>
-                        <h2 className={
-                            styles.recipeTitle1
-                        }>How to Cook It</h2>
-                        <p className={
-                            styles.recipeText
-                        }>
-                            This recipe was carefully designed and tested by{" "}
-                            {
-                            newRecipe.publisher
-                        }. Please check out directions at their
-                                                                                                                                                              website.
-                        </p>
-                        <button className={
-                            styles.recipeBtn
-                        }>Directions</button>
-                    </div>
-                </div>
-            )
-        } </React.Fragment>
+        <div className={styles.recipeIngredients}>
+          <ul className={styles.recipeIngredientsList}>
+            {newRecipe.ingredients.map((item, i) => (
+              <li className={styles.recipeList} key={i}>
+                <div className={styles.recipeIngredient}>{item}</div>
+              </li>
+            ))}{" "}
+          </ul>
+        </div>
+
+        <div className={styles.recipeDirections}>
+          <h2 className={styles.recipeTitle1}>How to Cook It</h2>
+          <p className={styles.recipeText}>
+            This recipe was carefully designed and tested by{" "}
+            {newRecipe.publisher}. Please check out directions at their website.
+          </p>
+
+          <a
+            target="_blank"
+            href={`${newRecipe.sourceUrl}`}
+            className={styles.recipeLink}
+          >
+            <button className={styles.recipeBtn}>Directions</button>
+          </a>
+        </div>
+      </div>
     );
+  } else {
+    recipeView = <p className={styles.recipeMsg}>Search now :)</p>;
+  }
+
+  return (
+    <React.Fragment>
+      {" "}
+      {shouldRecipeSpin ? <Spinner /> : recipeView}{" "}
+    </React.Fragment>
+  );
 };
 
 export default Recipe;
